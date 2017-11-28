3280b4a78b51d50fa473bffeb6241866bcf58d62
Fate <Fate@FATE-PC>
2017-11-29 00:35:30 +0900

    다고침 ㅋㅋㅋ Form5 보셈

--- WindowsApplication2/Form5.vb
+++ WindowsApplication2/Form5.vb
@@ -3,17 +3,17 @@ Public Class Form5
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim client As WebClient = New WebClient()
         Try
-            If CheckBox1.Checked Or CheckBox2.Checked Then
+            If CheckBox1.Checked And CheckBox2.Checked = True Then
                 Dim reply As String = client.DownloadString("http://optmusic.oa.to/user_add.php?username=" & TextBox1.Text & "&password=" & TextBox2.Text)
                 If (reply = "1") Then
                     MsgBox("회원가입 성공하였습니다.")
                 Else
                     MsgBox("회원가입에 실패하였습니다. " & reply, MsgBoxStyle.Information, "로그인 오류!")
                 End If
-            End If
-            If CheckBox1.Checked Or CheckBox2.Checked = False Then
+            Else
                 MsgBox("이용약관에 동의가 되어있지 않습니다.", MsgBoxStyle.Information, "로그인 오류!")
             End If
+
         Catch ex As Exception
             MsgBox("서버에 연결할 수 없습니다. " & ex.Message)
         End Try
